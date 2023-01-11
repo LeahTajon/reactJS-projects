225a2c05c752971c3a73654025aa0dfc2b97febd
Leah Tajon <leahtajon@gmail.com>
2023-01-12 09:09:29 +1000

    Add another react js example

--- first-react/index.js
+++ first-react/index.js
@@ -1,9 +1,26 @@
+// Example #1
 /* Try to write that 1-liner of React code again! This time,
 see if you can figure out how to render an <ul> with 2+ <li>s inside */
-ReactDOM.render(
-    <ul>
-        <li>JavaScript</li>
-        <li>Python</li>
-    </ul>,
-    document.getElementById('root')
-);
+
+// ReactDOM.render(
+//     <ul>
+//         <li>JavaScript</li>
+//         <li>Python</li>
+//     </ul>,
+//     document.getElementById('root')
+// );
+
+
+// Example #2
+/* Challenge - recreate the above line of code in vanilla JS by creating and appending* an h1 to our div#root (without using innerHTML)
+
+- create a new h1 element
+- Give it some textContent
+-Give it a class name of "header"
+-append it as a child of the div#root
+*/
+
+// const h1 = document.createElement('h1');
+// h1.textContent = 'Hello Again';
+// h1.className = 'header';
+// document.getElementById('root').append(h1);
