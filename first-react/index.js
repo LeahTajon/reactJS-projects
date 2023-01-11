e730bc24cce736bf4bfd83557d53a4075d8408d0
Leah Tajon <leahtajon@gmail.com>
2023-01-12 09:27:13 +1000

    Add another example

--- first-react/index.js
+++ first-react/index.js
@@ -24,3 +24,42 @@ see if you can figure out how to render an <ul> with 2+ <li>s inside */
 // h1.textContent = 'Hello Again';
 // h1.className = 'header';
 // document.getElementById('root').append(h1);
+
+// Example #3
+// const page = (
+//     <div>
+//         <h1 className='header'>This is JSX</h1>
+//         <p>This is a paragraph</p>
+//     </div>
+//     );
+
+// ReactDOM.render(
+//     page,
+//     document.getElementById('root')
+// );
+
+/* Challenge:
+
+Create a navbar in JSX:
+- Use the semantic `nav` element as the parent wrapper
+- Have an h1 element with the brand name of your "website"
+- Insert an unordered list for the other nav elements
+    - Inside the `ul`, have three `li`s for "Pricing", "About", and "Contact"
+    - Don't worry about styling yet - it'll just be plain-looking HTNK for now
+*/
+
+const nav = (
+    <nav>
+        <h1>Website</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(
+    nav,
+    document.getElementById('root')
+);
