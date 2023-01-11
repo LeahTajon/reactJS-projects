b30b9678c5f651ef132d272e5ae48f37f5949cb5
Leah Tajon <leahtajon@gmail.com>
2023-01-12 08:54:28 +1000

    Create first react js example

---
+++ first-react/index.js
@@ -0,0 +1,9 @@
+/* Try to write that 1-liner of React code again! This time,
+see if you can figure out how to render an <ul> with 2+ <li>s inside */
+ReactDOM.render(
+    <ul>
+        <li>JavaScript</li>
+        <li>Python</li>
+    </ul>,
+    document.getElementById('root')
+);
