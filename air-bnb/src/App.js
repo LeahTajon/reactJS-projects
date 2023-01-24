2c80f541cd63488fcb1f3c12a8552a86248c5da0
Leah Tajon <leahtajon@gmail.com>
2023-01-24 13:02:21 +1000

    Add JavaScript challenge

--- air-bnb/src/App.js
+++ air-bnb/src/App.js
@@ -2,15 +2,25 @@ import React from "react";
 
 
 import NavBar from "./Components/NavBar";
-import Hero from "./Components/Hero";
+//import Hero from "./Components/Hero";
 import Card from "./Components/Card";
+
+import katiePhoto from './images/katie-zaferes.png';
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Hero />
-      <Card />
+      {/* <Hero /> */}
+      <Card 
+        img={katiePhoto}
+        rating="5.0"
+        reviewCount={6}
+        country='USA'
+        title='Life Lessons with Katie Zaferes'
+        price={136}
+      />
+      
     </div>
   );
 }
