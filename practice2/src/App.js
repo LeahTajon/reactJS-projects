af9af2bb45dc099d90b8fd2b9cc52a88a01d9624
Leah Tajon <leahtajon@gmail.com>
2023-01-24 13:30:47 +1000

    Modify Joke component

--- practice2/src/App.js
+++ practice2/src/App.js
@@ -1,31 +1,17 @@
 import React from 'react';
 
 import Joke from '../src/Components/Joke'
+import JokesData from './Components/JokesData';
 
 function App() {
+  const jokeElement = JokesData.map(joke => {
+    return <Joke 
+              setup={joke.setup} 
+              punchline={joke.punchline}/>
+  })
   return (
     <div>
-      <Joke 
-        setup="Why did the tomato turn red?"
-        punchline="Because it saw the salad dressing!"
-      />
-      <Joke 
-        setup="What do you call a bear with no teeth?"
-        punchline="A gummy bear!"
-      />
-      <Joke 
-        setup="Why don't scientists trust atoms?"
-        punchline="Because they make up everything!"
-      />
-      <Joke 
-        setup="Why was the math book sad?"
-        punchline="Because it had too many problems."
-      />
-      <Joke 
-        setup="Why did the chicken cross the playground?"
-        punchline="To get to the other slide."
-      />
-      
+      {jokeElement}
     </div>
   );
 }
---
+++ practice2/src/Components/JokesData.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        setup: "Why did the tomato turn red?",
+        punchline: "Because it saw the salad dressing!"
+    },
+    {
+        setup: "What do you call a bear with no teeth?",
+        punchline: "A gummy bear!"
+    },
+    {
+        setup:"Why don't scientists trust atoms?",
+        punchline:"Because they make up everything!"
+    },
+    {
+        setup: "Why was the math book sad?",
+        punchline: "Because it had too many problems."
+    },
+    {
+        setup: "Why did the chicken cross the playground?",
+        punchline: "To get to the other slide."
+    }
+
+]
