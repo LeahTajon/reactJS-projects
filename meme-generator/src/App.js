b326d4aff5b15453049de5889846aef664cf29a1
Leah Tajon <leahtajon@gmail.com>
2023-01-29 07:17:24 +1000

    Create meme generator project

---
+++ meme-generator/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Header from './Components/Header';
+import Meme from './Components/Meme';
+
+function App() {
+  return (
+    <div>
+      <section>
+        <Header />  
+      </section>   
+      <section>
+        <Meme />  
+      </section>
+    </div>
+  );
+}
+
+export default App;
