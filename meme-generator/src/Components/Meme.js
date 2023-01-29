cdcd6c6481e436ed69495b099879d110e78fd34e
Leah Tajon <leahtajon@gmail.com>
2023-01-29 22:58:54 +1000

    Modify meme generator

--- meme-generator/src/Components/Meme.js
+++ meme-generator/src/Components/Meme.js
@@ -2,14 +2,24 @@ import React from 'react';
 import memesData from '../memesData';
 
 export default function Meme() {
-    const [memeImage, setMemeImage] = React.useState('')
+    const [meme, setMeme] = React.useState({
+        topText: '',
+        bottomText: '',
+        randomImage: 'http://i.imgflip.com/lbij.jpg'
+    })
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
     function getMemeImage() {
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
         
-        // const url = memesArray[randomNumber].url
-        setMemeImage(memesArray[randomNumber].url)
        
     }
 
@@ -31,7 +41,7 @@ export default function Meme() {
                     onClick={getMemeImage}>
                         Get a new meme image 🖼️
                 </button>
-                <img src={memeImage} className='meme--image' alt='funny'/>
+                <img src={meme.randomImage} className='meme--image' alt='funny'/>
 
             </div>
         </main>
