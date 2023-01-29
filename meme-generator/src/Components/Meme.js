e88b39bf9e1e7c21040d2045846e38c3c6753b7f
Leah Tajon <leahtajon@gmail.com>
2023-01-29 14:41:27 +1000

    Create counter

--- meme-generator/src/Components/Meme.js
+++ meme-generator/src/Components/Meme.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import memesData from '../memesData';
 
 export default function Meme() {
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        
+
+        
+    }
+
     return (
         <main>
-            <form className='form'>
+            <div className='form'>
                 <input 
                     type='text'
                     placeholder='Top text'
@@ -15,10 +25,11 @@ export default function Meme() {
                     className='form--input'
                 />
                 <button
-                    className='form--button'>
+                    className='form--button'
+                    onClick={getMemeImage}>
                         Get a new meme image 🖼️
                 </button>
-            </form>
+            </div>
         </main>
     )
 }
--- practice2/src/App.js
+++ practice2/src/App.js
@@ -1,20 +1,90 @@
 import React from 'react';
 
-import Joke from '../src/Components/Joke'
-import JokesData from './Components/JokesData';
-
 function App() {
-  const jokeElement = JokesData.map(joke => {
-    return <Joke 
-              setup={joke.setup} 
-              punchline={joke.punchline}/>
-  })
-  
+  /*
+    Challenge: Set up state to track our count (initial value is 0)
+  */
+
+  const [count, setCount] = React.useState(0)
+  /*
+    Note: if you ever need the old value of state to help you determine the new value of state, you should pass a callback function to your state setter function instead of using state directly. This callback function will receive the old value of state as its parameter, which you can then use to determine your new value of state.
+  */
+  function add() {
+    setCount(prevCount => prevCount + 1)
+  }
+
+  function subtract() {
+    setCount(prevCount => prevCount - 1)
+  }
+
   return (
-    <div>
-      {jokeElement}
+    <div className='counter'>
+      <button 
+        className='counter--minus'
+        onClick={subtract}>-</button>
+
+      <div className='counter--count'>
+        <h1>{count}</h1>
+      </div>
+
+      <button 
+        className='counter--plus'
+        onClick={add}>+</button>
     </div>
-  );
+  )
 }
 
+
 export default App;
+
+
+
+
+
+
+
+// PRACTICE #1
+
+// import Joke from '../src/Components/Joke'
+// import JokesData from './Components/JokesData';
+
+// function App() {
+//   const jokeElement = JokesData.map(joke => {
+//     return <Joke 
+//               setup={joke.setup} 
+//               punchline={joke.punchline}/>
+//   })
+  
+//   return (
+//     <div>
+//       {jokeElement}
+//     </div>
+//   );
+// }
+
+
+// PRACTICE #2
+
+// function App() {
+  /*
+    Challenge:
+    1. Create a function called 'handleClick' that runs setIsImportant("No")
+    2. Add a click event listener to the div.state--value that runs 'handleClick' when the div is clicked.
+  */
+
+
+//    const [isImportant, setIsImportant] = React.useState("Yes")
+
+//    function handleClick() {
+//     setIsImportant("Huh?")
+//    }
+   
+//    return (
+//     <div className='state'>
+//       <h1 className='state--title'>Is state important to know?</h1>
+//       <div className='state--value' onClick={handleClick}>
+//         <h1>{isImportant}</h1>
+//       </div>
+//     </div>
+//    )
+// }
