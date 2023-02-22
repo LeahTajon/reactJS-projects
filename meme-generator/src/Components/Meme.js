3abe735265cc6796a7a1896fd992a5396cba34e6
Leah Tajon <leahtajon@gmail.com>
2023-02-22 10:03:09 +1000

    Modify react js projects

--- meme-generator/src/Components/Meme.js
+++ meme-generator/src/Components/Meme.js
@@ -8,8 +8,8 @@ export default function Meme() {
         randomImage: 'http://i.imgflip.com/lbij.jpg'
     })
 
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
-
+    //const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    console.log(meme.bottomText); 
     function getMemeImage() {
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
@@ -18,9 +18,15 @@ export default function Meme() {
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
+        }))  
+    }
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
         }))
-        
-       
     }
 
     return (
@@ -30,19 +36,28 @@ export default function Meme() {
                     type='text'
                     placeholder='Top text'
                     className='form--input'
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
                 />
                 <input 
                     type='text'
                     placeholder='Bottom text'
                     className='form--input'
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
                 />
                 <button
                     className='form--button'
                     onClick={getMemeImage}>
                         Get a new meme image 🖼️
                 </button>
-                <img src={meme.randomImage} className='meme--image' alt='funny'/>
-
+                <div className="meme">
+                    <img src={meme.randomImage} className='meme--image' alt='funny'/>
+                    <h2 className="meme--text top">{meme.topText}</h2>
+                    <h2 className="meme--text bottom">{meme.bottomText}</h2>
+                </div>
             </div>
         </main>
     )
